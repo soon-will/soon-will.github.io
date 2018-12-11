0da4e30cc98c4e6868fc9d9a0f7b8053b3cbd638
soon-will <songwei_zhang@foxmail.com>
2018-12-11 17:46:57 +0800

    Site updated: 2018-12-11 17:46:50

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Dec 10 2018 11:06:46 GMT+0800 (GMT+08:00)
+// build time:Tue Dec 11 2018 17:46:40 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
